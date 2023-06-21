b56816364932e776397c5beef838f0dca1aa9e23
AnwarK.code <115826335+AnwarKXC@users.noreply.github.com>
2023-06-21 20:45:42 +0300

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
+  description: 'Reddit clone by Anwar .',
 }
 
 export default function RootLayout({
